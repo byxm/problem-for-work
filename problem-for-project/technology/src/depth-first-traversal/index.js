c9f5bba1b2f6b89223da981f96f7dca0976efb16
陈攀宇 <panyuximeng@163.com>
2020-10-29 23:22:19 +0800

    feat: 查找对象路径优化解法

--- problem-for-project/technology/src/depth-first-traversal/index.js
+++ problem-for-project/technology/src/depth-first-traversal/index.js
@@ -1,24 +1,41 @@
-const deepTraversal = (traversalObj, isIterateArray = false) => {
-  const initArr = [];
-  excuteTraversal(traversalObj, initArr, [], isIterateArray);
-  return initArr;
-};
+// const deepTraversal = (traversalObj, isIterateArray = false) => {
+//   const initArr = [];
+//   excuteTraversal(traversalObj, initArr, [], isIterateArray);
+//   return initArr;
+// };
 
-const excuteTraversal = (traversalObj, initArr, keyPath, isIterateArray) => {
-  const keys = Object.keys(traversalObj);
+// const excuteTraversal = (traversalObj, initArr, keyPath, isIterateArray) => {
+//   const keys = Object.keys(traversalObj);
+//   keys.forEach((k) => {
+//     keyPath.push(k);
+//     const isIteratorArray = !isIterateArray
+//       ? !(traversalObj[k] instanceof Array)
+//       : true;
+//     if (typeof traversalObj[k] === "object" && isIteratorArray) {
+//       excuteTraversal(traversalObj[k], initArr, keyPath, isIterateArray);
+//       keyPath.pop();
+//     } else {
+//       initArr.push(keyPath.join("."));
+//       keyPath.pop();
+//     }
+//   });
+// };
+
+// 做完leetcode-257号问题后的优化解法
+const deepTraversal = (traversalObj, key) => {
+  const res = [];
+  if (key && typeof traversalObj[key] !== "object") {
+    res.push(key);
+    return res;
+  }
+  const keys = Object.keys(key ? traversalObj[key] : traversalObj);
   keys.forEach((k) => {
-    keyPath.push(k);
-    const isIteratorArray = !isIterateArray
-      ? !(traversalObj[k] instanceof Array)
-      : true;
-    if (typeof traversalObj[k] === "object" && isIteratorArray) {
-      excuteTraversal(traversalObj[k], initArr, keyPath, isIterateArray);
-      keyPath.pop();
-    } else {
-      initArr.push(keyPath.join("."));
-      keyPath.pop();
+    const deepPaths = deepTraversal(key ? traversalObj[key] : traversalObj, k);
+    for (let i = 0; i < deepPaths.length; i++) {
+      res.push(`${key ? `${key}.` : ""}${deepPaths[i]}`);
     }
   });
+  return res;
 };
 
 module.exports = {
